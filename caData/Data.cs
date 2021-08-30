ee69831a6e96cd5c42ba2a9c769bb8c8cbc081a8
Nicole <Nicole@DESKTOP-S1FUTDS>
2021-08-30 09:57:21 -0300

    Adicionar arquivos de projeto.

---
+++ caData/Data.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caData
+{
+    class Data
+    {
+        private int dia;
+        private int mes;
+        private int ano;
+
+        public Data(int _dia, int _mes, int _ano)
+        {
+            dia = _dia;
+            mes = _mes;
+            ano = _ano;
+            string date;
+            date = String.Format("{0:00}:{1:00}:{2:0000}", dia, mes, ano);
+        }
+
+        public int Compara(Data d)
+        {
+            if (d.ano < ano)
+            {
+                return 1;
+            }
+            else if (d.ano > ano)
+            {
+                return -1;
+            }
+            else if (d.mes < mes)
+            {
+                return 1;
+            }
+            else if (d.mes > mes)
+            {
+                return -1;
+            }
+            else if (d.dia < dia)
+            {
+                return 1;
+            } 
+            else if(d.dia > dia)
+            {
+                return -1;
+            }
+            else { return 0;  }
+
+        }
+        public int GetDia()
+        {
+            return dia;
+        }
+        public int GetMes()
+        {
+            return mes;
+        }
+        public int GetAno()
+        {
+            return ano;
+        }
+        public string GetMesExtenso()
+        {
+            if(mes == 1)
+            {
+                return "janeiro";
+            }
+            else if(mes == 2)
+            {
+                return "fevereiro";
+            }
+            else if (mes == 3)
+            {
+                return "março";
+            }
+            else if (mes == 4)
+            {
+                return "abril";
+            }
+            else if (mes == 5)
+            {
+                return "maio";
+            }
+            else if (mes == 6)
+            {
+                return "junho";
+            }
+            else if (mes == 7)
+            {
+                return "julho";
+            }
+            else if (mes == 8)
+            {
+                return "agosto";
+            }
+            else if (mes == 9)
+            {
+                return "setembro";
+            }
+            else if (mes == 10)
+            {
+                return "outubro";
+            }
+            else if (mes == 11)
+            {
+                return "novembro";
+            }
+            else { return "dezembro"; }
+        }
+
+        public Boolean IsBissexto()
+        {
+            if((ano % 4) == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public Data Clone()
+        {
+            return new Data(dia, mes, ano);
+        }
+    }
+
+}
---
+++ caData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace caData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data(2, 9, 2021);
+            Data data2 = new Data(30, 9, 2000);
+            data2 = data.Clone();
+            Console.WriteLine(data.Compara(data2));
+        }
+    }
+}
